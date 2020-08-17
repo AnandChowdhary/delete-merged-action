c945a9527ed18d6e428afee7f46df6ff0cfd1208
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-18 00:45:46 +0530

    :construction: Add PR test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { getInput, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
-import {} from "@octokit/webhooks";
+import { EventPayloads } from "@octokit/webhooks";
 
 const token =
   getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
@@ -15,7 +15,10 @@ export const run = async () => {
   if (!context.payload.pull_request)
     return console.log("No pull request found");
 
-  const pullRequest = context.payload.pull_request;
+  const pullRequest = (context as any).payload.pull_request
+    .body as EventPayloads.WebhookPayloadPullRequest;
+
+  console.log("Pull number", pullRequest.pull_request);
 };
 
 run()
