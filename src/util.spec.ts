6f426741d6040bd834fc14e244155a5cc9081598
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-18 17:05:49 +0530

    :white_check_mark: Add tests for util

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -11,3 +11,30 @@ describe("should merge default", () => {
     expect(shouldMerge("master", defaultValue)).toBeFalsy();
   });
 });
+
+describe("should merge with input", () => {
+  test("example-branch", () => {
+    expect(shouldMerge("example-branch", "example-branch")).toBeTruthy();
+  });
+  test("example-branch", () => {
+    expect(shouldMerge("example-branch-1", "example-branch")).toBeFalsy();
+  });
+  test("example-*", () => {
+    expect(shouldMerge("example-branch", "example-*")).toBeTruthy();
+  });
+  test("example-*", () => {
+    expect(shouldMerge("example-branch-1", "example-*")).toBeTruthy();
+  });
+  test("example-*, hello-*", () => {
+    expect(shouldMerge("example-branch", "example-*")).toBeTruthy();
+  });
+  test("example-*, hello-*", () => {
+    expect(shouldMerge("hello-world", "example-*")).toBeFalsy();
+  });
+  test("example-*,hello-*", () => {
+    expect(shouldMerge("example-branch", "example-*")).toBeTruthy();
+  });
+  test("example-*,hello-*", () => {
+    expect(shouldMerge("hello-world", "example-*")).toBeFalsy();
+  });
+});
