0b41f4c572d5821e56b1d2295ee1286727a0c6fd
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-26 16:57:05 +0530

    :loud_sound: Add logs for should delete

--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,12 @@ export const defaultValue = "!master,!main,*";
  * @param rules - List of glob rules
  */
 export const shouldMerge = (branch: string, rules?: string) => {
+  console.log("Start: Debug should delete");
   const branches = (rules || "").split(",").map((branch) => branch.trim());
-  let shouldMerge = branches.every((rule) => minimatch(branch, rule));
+  let shouldMerge = branches.every((rule) => {
+    console.log(branch, rule, minimatch(branch, rule));
+    return minimatch(branch, rule);
+  });
+  console.log("End: Debug should delete, result:", shouldMerge);
   return shouldMerge;
 };
